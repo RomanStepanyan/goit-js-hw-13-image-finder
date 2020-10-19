9f5377236fbf0662e17cc4f190b93528a87b83df
RomanStepanyan <stepik10@gmail.com>
2020-10-20 01:39:07 +0300

    LigthBox modal window is added and corrected

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import './styles.css';
+import './scss/lightBox.scss'
 
 import './js/fetch-images'
 
--- src/js/fetch-images.js
+++ src/js/fetch-images.js
@@ -2,10 +2,9 @@
 import debounce from 'lodash.debounce';
 import refs from './refs'
 import makeImageMarkup from './image-markup'
-// import fetchImg from './fetch-images'
 import apiService from './apiService'
 
-refs.serch.addEventListener('input', debounce(getInputQuery, 600))
+refs.serch.addEventListener('input', debounce(getInputQuery, 700))
 
 function getInputQuery(event){
     event.preventDefault();
@@ -35,16 +34,10 @@ import * as basicLightbox from 'basiclightbox'
 
 
 refs.gallery.addEventListener('click', event => {
-    
-    console.log(event);
-    console.log(event.target);
     const imageSrc = event.target.currentSrc;
     const imageHeight = event.target.naturalHeight;
     const imageWidth = event.target.naturalWidth;
-    console.log(imageSrc);
     const instance = basicLightbox.create(`<img src="${imageSrc}" width="${imageWidth}" height="${imageHeight}">`)
     instance.show()
 })
 
-
-// document.body.scrollHeight
