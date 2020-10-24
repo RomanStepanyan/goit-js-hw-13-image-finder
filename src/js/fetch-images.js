232d963e9f90004f457e04a2322ab580e4f6bc1d
RomanStepanyan <stepik10@gmail.com>
2020-10-24 16:01:34 +0300

    Some problems with Button appearance have been resoled

--- src/js/fetch-images.js
+++ src/js/fetch-images.js
@@ -1,43 +1,50 @@
-
 import debounce from 'lodash.debounce';
-import refs from './refs'
-import makeImageMarkup from './image-markup'
-import apiService from './apiService'
+import refs from './refs';
+import makeImageMarkup from './image-markup';
+import apiService from './apiService';
 
-refs.serch.addEventListener('input', debounce(getInputQuery, 700))
+refs.serch.addEventListener('input', debounce(getInputQuery, 700));
 
-function getInputQuery(event){
-    event.preventDefault();
-    apiService.query = event.target.value   
-    refs.gallery.innerHTML = "";
-    refs.serch.reset();
-    apiService.resetPage();
-    refs.btnLoadMore.classList.add('is-hidden');
-    apiService.fetchImg().then(hits => {
-        makeImageMarkup(hits);
-        refs.btnLoadMore.classList.remove('is-hidden');
+function getInputQuery(event) {
+  event.preventDefault();
+  apiService.query = event.target.value;
+  refs.gallery.innerHTML = '';
+  refs.serch.reset();
+  apiService.resetPage();
+  refs.btnLoadMore.classList.add('is-hidden');
+  apiService
+    .fetchImg()
+    .then(hits => {
+      makeImageMarkup(hits); 
+      refs.btnLoadMore.classList.remove('is-hidden');
+      if (hits.length < 12) {
+        refs.btnLoadMore.classList.add('is-hidden');
+        }
     })
 }
 
-refs.btnLoadMore.addEventListener('click', ()  =>{
-    apiService.fetchImg().then(hits => {
-        makeImageMarkup(hits);
-        window.scrollTo({
-            top: document.documentElement.offsetHeight,
-            behavior: 'smooth',
-          });
-    })
- 
-})
+refs.btnLoadMore.addEventListener('click', () => {
+  apiService.fetchImg().then(hits => {
+    makeImageMarkup(hits);
+    if (hits.length < 12) {
+        refs.btnLoadMore.classList.add('is-hidden');
+    }
+    window.scrollTo({
+      top: document.documentElement.offsetHeight,
+      behavior: 'smooth',
+    });
+  });
 
-import * as basicLightbox from 'basiclightbox'
+});
 
+import * as basicLightbox from 'basiclightbox';
 
 refs.gallery.addEventListener('click', event => {
-    const imageSrc = event.target.currentSrc;
-    const imageHeight = event.target.naturalHeight;
-    const imageWidth = event.target.naturalWidth;
-    const instance = basicLightbox.create(`<img src="${imageSrc}" width="${imageWidth}" height="${imageHeight}">`)
-    instance.show()
-})
-
+  const imageSrc = event.target.currentSrc;
+  const imageHeight = event.target.naturalHeight;
+  const imageWidth = event.target.naturalWidth;
+  const instance = basicLightbox.create(
+    `<img src="${imageSrc}" width="${imageWidth}" height="${imageHeight}">`,
+  );
+  instance.show();
+});
